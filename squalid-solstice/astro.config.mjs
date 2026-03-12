21cfd63f91c46bdceb002d53e14836476c9ba1a3
孙琪超 <chaolanguage345@gmail.com>
2026-03-12 17:33:13 +0900

    1

--- squalid-solstice/astro.config.mjs
+++ squalid-solstice/astro.config.mjs
@@ -10,10 +10,11 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: '知识点学习',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
+						
 					],
 				},
 				{
